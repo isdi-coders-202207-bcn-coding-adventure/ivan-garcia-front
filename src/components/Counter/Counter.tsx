0e30f1bff5bedf2ff5d41d345f0daf217201f68d
Ivan Garcia <ivanggv2@gmail.com>
2022-08-22 15:12:17 +0200

    Created component Counter

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,12 @@
+const Counter = () => {
+  return (
+    <ul className="field-list">
+      <li className="field">Days</li>
+      <li className="field">Hours</li>
+      <li className="field">Minutes</li>
+      <li className="field">Seconds</li>
+    </ul>
+  );
+};
+
+export default Counter;
