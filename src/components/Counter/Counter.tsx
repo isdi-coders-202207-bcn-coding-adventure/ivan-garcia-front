0688215897985da7cebdf6c2cf5c0c5de27159c8
Ivan Garcia <ivanggv2@gmail.com>
2022-08-22 15:22:36 +0200

    Added BEM to the component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,10 +1,25 @@
 const Counter = () => {
   return (
     <ul className="field-list">
-      <li className="field">Days</li>
-      <li className="field">Hours</li>
-      <li className="field">Minutes</li>
-      <li className="field">Seconds</li>
+      <li className="field-list__field">
+        <div className="field-list__time">Days</div>
+        <h2 className="field__title">Days</h2>
+      </li>
+
+      <li className="field-list__field">
+        <div className="field-list__time">Hours</div>
+        <h2 className="field__title">Hours</h2>
+      </li>
+
+      <li className="field-list__field">
+        <div className="field-list__time">Minutes</div>
+        <h2 className="field__title">Minutes</h2>
+      </li>
+
+      <li className="field-list__field">
+        <div className="field-list__time">Seconds</div>
+        <h2 className="field__title">Seconds</h2>
+      </li>
     </ul>
   );
 };
